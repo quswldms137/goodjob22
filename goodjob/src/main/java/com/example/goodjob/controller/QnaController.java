a54476f062a8889ac09421011f8ebe687e1cd4dc
DESKTOP-32G3IVT\ycjeo <ycjeong99@gmail.com>
2024-06-15 22:00:32 +0900

    문의내역 페이지 취소 기능 포함, 마이페이지 홈 기능 구현 (화면 구성 설정 필요)

--- goodjob/src/main/java/com/example/goodjob/controller/QnaController.java
+++ goodjob/src/main/java/com/example/goodjob/controller/QnaController.java
@@ -2,7 +2,10 @@ package com.example.goodjob.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.goodjob.dto.ServiceQuestionDto;
 import com.example.goodjob.service.ServiceQuestionService;
 
-@CrossOrigin("http://localhost:9991/")
+@CrossOrigin("http://localhost:9991")
 @RestController
 @RequestMapping("/api/qna")
 public class QnaController {
@@ -23,8 +26,30 @@ public class QnaController {
 	}
 	
 	@GetMapping("/{username}")
-	public String getQna(@PathVariable("username") String username) {
+	public ResponseEntity<List<ServiceQuestionDto>> getQna(@PathVariable("username") String username) {
 		
-		return "행복하세요";
+		List<ServiceQuestionDto> result = serviceQuestionService.getQnaList(username);
+		
+		if(!result.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(result);
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(result);
+	}
+	
+	@DeleteMapping("/{cs_no}")
+	public ResponseEntity<String> deleteQna(@PathVariable("cs_no") Long cs_no) {
+		
+		int result = serviceQuestionService.deleteQna(cs_no);
+		
+		if(result > 0) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body("문의내역이 성공적으로 삭제되었습니다.");
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body("문의내역 삭제가 실패했습니다.");
 	}
 }
