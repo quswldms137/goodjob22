24ca23026f544edcdb10664d8224e654e6b2edf1
Bae seowon <bsw0825@naver.com>
2024-06-17 21:40:03 +0900

    채용공고리스트 페이지 구현중(리스트는 완료 / 버튼은 놉)

--- goodjob/src/main/java/com/example/goodjob/dao/IRecruitListDao94.java
+++ goodjob/src/main/java/com/example/goodjob/dao/IRecruitListDao94.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.example.goodjob.dto.RecruitAllDto94;
 import com.example.goodjob.dto.RecruitDto;
 
 @Mapper
 public interface IRecruitListDao94 {
+	//해당기업의 채용공고 리스트 
 	public List<RecruitDto> getRecruitListDto(@Param("com_no") Long com_no);
+
+	//전체채용공고리스트
+	public List<RecruitAllDto94> getAllRecruitDto();
+	
 }
 
