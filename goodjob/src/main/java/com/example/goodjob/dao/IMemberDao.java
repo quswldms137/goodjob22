c72222843e76d3eefcf764796444c2bc95977ac1
Jeong HanYeon <gksdusdl5@gmail.com>
2024-06-19 08:52:04 +0900

    MemberDao 오류 (메서드 중복) 수정

--- goodjob/src/main/java/com/example/goodjob/dao/IMemberDao.java
+++ goodjob/src/main/java/com/example/goodjob/dao/IMemberDao.java
@@ -14,12 +14,11 @@ public interface IMemberDao {
 	public Long getMem_no(@Param("username") String username);
 	public void update(@Param("memberDto") MemberDto memberDto);
 	public void deleteInformation(@Param("mem_no") Long mem_no);
-<<<<<<< HEAD
 	
-	public String findByMem_no(@Param("mem_no") Long mem_no);
-=======
+	public String findMem_nameWithMem_no(@Param("mem_no") Long mem_no);
+	
 	public List<MemberDto> getMemberDtoList(@Param("start") int start);
 	public int count();
 	public MemberDto findByMem_no(@Param("mem_no") Long mem_no);
->>>>>>> 526da82355ada9429f294493e912e56c48975b5f
+
 }
